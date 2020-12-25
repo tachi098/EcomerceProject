23275e5ab423b1f9081cd9dcf95eaadcf9e208cb
Kitajima2910 <kitajima2910@gmail.com>
2020-12-25 18:24:35 +0700

    hoaipx/20201225-01: Add: - QRCode, Barcode - System.Drawing.Common - ZXing.Net

---
+++ Controllers/BarcodesController.cs
@@ -0,0 +1,24 @@
+﻿using EcomerceProject.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomerceProject.Controllers
+{
+    public class BarcodesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string barcode)
+        {
+            ViewBag.BarcodeImage = Helper.Barcode.Set(barcode);
+            return View();
+        }
+    }
+}
