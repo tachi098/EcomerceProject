ebc107d4587537d1b6ac7e578f4a15103f649ff1
Kitajima2910 <kitajima2910@gmail.com>
2020-12-25 23:58:32 +0700

    hoaipx/20201225-02: Add: - CustomAssemblyLoadContext - DinkToPdf - libwkhtmltox (dll, dylib, so) Modified: - ProductController, _Layout, Startup

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using DocumentFormat.OpenXml.Wordprocessing;
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using DocumentFormat.OpenXml.Wordprocessing;
 using EcomerceProject.Entities;
 using EcomerceProject.Repositories;
 using EcomerceProject.Services.Interfaces;
@@ -6,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,10 +18,13 @@ namespace EcomerceProject.Controllers.Admin
     {
         private IProductServices services;
         private ApplicationContext context;
-        public ProductController(IProductServices services, ApplicationContext context)
+        private IConverter _converter;
+
+        public ProductController(IProductServices services, ApplicationContext context, IConverter converter)
         {
             this.services = services;
             this.context = context;
+            _converter = converter;
         }
         public IActionResult Index()
         {
@@ -43,6 +49,40 @@ namespace EcomerceProject.Controllers.Admin
             int pageSize = 2;
             return View(await PaginatedList<Product>.CreateAsync(products.AsNoTracking(), page ?? 1, pageSize));
             //return PartialView("_UserIndex", await PaginatedList<Product>.CreateAsync(products.AsNoTracking(), page ?? 1, pageSize));
+        }
+
+
+        public IActionResult CreatePDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report",
+                //Out = @"D:\PDFCreator\Report.pdf"
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                //HtmlContent = "<h1>Hello PDF</h1>",
+                //Page = "http://localhost:52743/",
+                Page = "http://localhost:52743/User/Login",
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+            _converter.Convert(pdf);
+
+            var file = _converter.Convert(pdf);
+            return File(file, "application/pdf", "Report.pdf");
         }
 
     }
